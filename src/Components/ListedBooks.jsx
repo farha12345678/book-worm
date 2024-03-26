e4d750e858ec84f841f77fe513f35936ced2f29c
farha12345678 <sabikunnaharfarha01@gmail.com>
2024-03-26 17:50:02 +0600

    forth commit

--- src/Components/ListedBooks.jsx
+++ src/Components/ListedBooks.jsx
@@ -1,10 +1,18 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const ListedBooks = () => {
+    const [tableIndex , setTableIndex] = useState(0)
     return (
         <div>
-           <div className="bg-base-300 rounded-2xl pt-8 mx-auto h-24 text-center">
-            <h1 className="text-3xl font-bold">Books</h1>
-           </div>
+            <div className="bg-base-300 rounded-2xl pt-8 mx-auto h-24 text-center">
+                <h1 className="text-3xl font-bold">Books</h1>
+            </div>
+            {/* tab */}
+            <div role="tablist" className="tabs tabs-lifted text-center flex ml-20">
+                <Link onClick={() => setTableIndex(0)} role="tab" className={`${tableIndex === 0? 'tab-active':'tab' }`}>Read Books</Link>
+                <Link onClick={() => setTableIndex(1)} role="tab" className={`${tableIndex === 1? 'tab-active':'tab' }`}>Wishlist Books</Link>
+            </div>
         </div>
     );
 };
